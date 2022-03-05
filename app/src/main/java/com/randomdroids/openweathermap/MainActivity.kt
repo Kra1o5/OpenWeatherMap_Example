3424c57720d34a925fba6d41a5cefc2d45f9355a
Alejandro López <kra1o5x@gmail.com>
2022-03-05 12:49:36 +0100

    Init the project

---
+++ app/src/main/java/com/randomdroids/openweathermap/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.randomdroids.openweathermap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
